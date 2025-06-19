4153c9ea72ec28c9ee16d607fb3bf9cb486b3c4f
Pedro A. Pajarillo Jr <pedropa140@gmail.com>
2025-06-19 00:44:59 -0400

    Minor changes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        blackletter: ['"UnifrakturCook"', 'serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
   darkMode: 'class',
